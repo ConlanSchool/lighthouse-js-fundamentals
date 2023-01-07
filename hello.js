52c9bca69af61401d1e5ae182bd5b6add398d616
ConlanSchool <conlan.school@gmail.com>
2023-01-07 15:38:25 -0800

    declaring and calling functions practice

---
+++ hello.js
@@ -0,0 +1,16 @@
+const sayHi = function(){
+  console.log("Ohio!");
+
+}
+
+sayHi();
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+
